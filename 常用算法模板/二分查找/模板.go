6431affe7d29e0ecff17b1803ffa5df466119edc
chenbinbin <chenbinbin@didichuxing.com>
2021-02-04 21:44:08 +0800

    二分查找

--- 常用算法模板/二分查找/模板.go
+++ 常用算法模板/二分查找/模板.go
@@ -1,119 +1,52 @@
 package main
 import "fmt"
 
-/*
-并查集，判连通用
-*/
-type UnionFindSet struct {
-	father  []int // 存储结点的父亲
-	height  []int // 存储结点的父亲
-	nodeNum int   // 总结点个数
+
+func abs(a int) int {
+	if a<0 {return -a}
+	return a
+}
+func checkFunc(houses []int, heaters []int, radius int) bool {
+	i, j:=0,0
+	for ;i<len(houses) && j<len(heaters); {
+		if abs(houses[i] - heaters[j])<=radius{
+			i++
+		}else {j++}
+	}
+	return i==len(houses)
 }
 
-func (us *UnionFindSet) InitUnionSet(n int) {
-	us.nodeNum = n+1 // 不加也可以，有人喜欢以0开头
-	us.father = make([]int, us.nodeNum)
-	us.height = make([]int, us.nodeNum)
-	for i, _ := range us.father {
-		us.father[i] = i
-		us.height[i] = 1
-	}
-}
-
-// 查询父结点
-func (us *UnionFindSet) Find(x int) int {
-	for us.father[x] != x {
-		x = us.father[x]
-	}
-	return x
-}
-
-//合并结点
-func (us *UnionFindSet) Union(x, y int) bool {
-	x = us.Find(x)
-	y = us.Find(y)
-	if x == y {
-		return false
-	}
-	us.father[x] = y
-	return true
-}
-
-func (us *UnionFindSet) FindV2(x int) int {
-	root := x // 保存好路径上的头结点
-	for us.father[root] != root {
-		root = us.father[root]
-	}
-	/*
-	从头结点开始一直往根上遍历
-	把所有结点的father直接指向root。
-	*/
-	for us.father[x] != x {
-		// 一定要先保存好下一个结点，下一步是要对us.father[x]进行赋值
-		temp := us.father[x]
-		us.father[x] = root
-		x = temp
-	}
-
-	return root
-}
-
-/*
-需要加入height[]属性，初始化为1.
-*/
-//合并结点
-func (us *UnionFindSet) UnionV2(x, y int) bool {
-	x = us.Find(x)
-	y = us.Find(y)
-	if x == y {
-		return false
-	}
-	if us.height[x]<us.height[y] {
-		us.father[x]=y
-	} else if us.height[x]>us.height[y] {
-		us.father[y]=x
-	} else {
-		us.father[x] = y
-		us.height[y]++
-	}
-	return true
-}
-
-
-func findCircleNum(M [][]int) int {
-	us := &UnionFindSet{}
-	us.InitUnionSet(len(M))
-	for i,row:=range M{
-		for j,isFriend :=range row {
-			if isFriend==1 {us.UnionV2(i,j)} // 是朋友则合并
+func findRadius(houses []int, heaters []int) int {
+	// 排序
+	sort.Slice(houses, func(i, j int) bool {
+		return houses[i]<houses[j]
+	})
+	sort.Slice(heaters, func(i, j int) bool {
+		return heaters[i]<heaters[j]
+	})
+	best := int(10e9)
+	left, right := 0, int(10e9)
+	for left<= right {
+		mid := (right+left)>>1
+		if checkFunc(houses, heaters, mid) { // 可行解，贪心查看有没有更好解, 区间往小移
+			best, right = mid, mid-1
+		} else {
+			left = mid+1
 		}
 	}
 
-	friendCircle :=0
-	for i:=0;i<len(M);i++ {
-		if us.FindV2(i)==i { friendCircle++} // 查看集合代表个数
-	}
-
-	return friendCircle
+	return best
 }
+
 /*
-[[1,1,0],[1,1,0],[0,0,1]]
-[[1]]
-[[1,0,0],[0,1,0],[0,0,1]]
-[[1,0,1],[0,1,0],[1,0,1]]
-[[1,0,1,0],[0,1,0,0],[1,0,1,1],[0,0,1,1]]
-*/
-
-func main() {
-	us:= &UnionFindSet{}
-	us.InitUnionSet(11)
-	us.UnionV2(1, 2)
-	us.UnionV2(10, 9)
-	us.UnionV2(7,8)
-	us.UnionV2(8,3)
-	us.UnionV2(9,8)
-
-	for i:=0;i<=10; i++  {
-		fmt.Println(i, us.FindV2(i))
-	}
-}
+[1,2,3]
+[2]
+[2]
+[2]
+[1,2,3,4]
+[1,4]
+[1,5]
+[2]
+[4,3,2,1]
+[1,4]
+*/}
